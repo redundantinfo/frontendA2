0a62e40394b6dcc9189ef6f1ad1697f06d86bd0a
Simon Eriksson <simon_eriksson_90@outlook.com>
2023-06-20 08:58:38 +0200

    qweawsd

--- src/components/SeatSelection.jsx
+++ src/components/SeatSelection.jsx
@@ -1,5 +1,10 @@
 /*
 This component will display a graphic view of the auditorium and allow users to select available seats for a specific screening. 
-It will receive the screening ID as a prop and fetch the seat availability data from the backend API.
-The component should handle seat selection logic and calculate the total price based on the number of visitors and ticket prices for different age groups.
+It will receive the screening ID and fetch the seat availability data from the backend API.
+The component should handle seat selection and calculate the total price based on the number of visitors and ticket prices for different age groups.
 */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getSeats } from '../services/api';
+
+// use getSeats to fetch the seat data from the backend API, display the seat data in a table, and allow users to select seats.
--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,14 @@
 Handle API requests using the Fetch API
 */
 
-const BASE_URL = 'https://api.example.com';
-
-export async function fetchMovies() {
-  const response = await fetch(`${BASE_URL}/movies`);
+export async function getSeats() {
+  const response = await fetch("/api/seats");
   const data = await response.json();
   return data;
 }
 
-export async function fetchScreenings(movieId) {
-  const response = await fetch(`${BASE_URL}/movies/${movieId}/screenings`);
+export async function getSeatById(id) {
+  const response = await fetch(`/api/seats/${id}`);
   const data = await response.json();
   return data;
 }
